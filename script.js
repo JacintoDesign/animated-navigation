623cdf1878b90eb1ee194eb02d113974b7c1f6ee
JACINTO WONG <jacinto.wong@cbc.ca>
2020-05-12 10:53:43 -0400

    final production code

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
-const menuBars = document.getElementById("menu-bars");
-const overlay = document.getElementById("overlay");
-const nav1 = document.getElementById("nav-1");
-const nav2 = document.getElementById("nav-2");
-const nav3 = document.getElementById("nav-3");
-const nav4 = document.getElementById("nav-4");
-const nav5 = document.getElementById("nav-5");
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const nav4 = document.getElementById('nav-4');
+const nav5 = document.getElementById('nav-5');
 
 function toggleNav() {
   // Toggle: Menu Bars Open/Closed
-  menuBars.classList.toggle("change");
+  menuBars.classList.toggle('change');
   // Toggle: Menu Active
-  overlay.classList.toggle("overlay-active");
-  if (overlay.classList.contains("overlay-active")) {
+  overlay.classList.toggle('overlay-active');
+  if (overlay.classList.contains('overlay-active')) {
     // Animate In - Overlay
-    overlay.classList.remove("overlay-slide-left");
-    overlay.classList.add("overlay-slide-right");
+    overlay.classList.remove('overlay-slide-left');
+    overlay.classList.add('overlay-slide-right');
     // Animate In - Nav Items
-    nav1.classList.remove("slide-in-nav-item-reverse");
-    nav1.classList.add("slide-in-nav-item");
-    nav2.classList.remove("slide-in-nav-item-delay-1-reverse");
-    nav2.classList.add("slide-in-nav-item-delay-1");
-    nav3.classList.remove("slide-in-nav-item-delay-2-reverse");
-    nav3.classList.add("slide-in-nav-item-delay-2");
-    nav4.classList.remove("slide-in-nav-item-delay-3-reverse");
-    nav4.classList.add("slide-in-nav-item-delay-3");
-    nav5.classList.remove("slide-in-nav-item-delay-4-reverse");
-    nav5.classList.add("slide-in-nav-item-delay-4");
+    nav1.classList.remove('slide-in-nav-item-reverse');
+    nav1.classList.add('slide-in-nav-item');
+    nav2.classList.remove('slide-in-nav-item-delay-1-reverse');
+    nav2.classList.add('slide-in-nav-item-delay-1');
+    nav3.classList.remove('slide-in-nav-item-delay-2-reverse');
+    nav3.classList.add('slide-in-nav-item-delay-2');
+    nav4.classList.remove('slide-in-nav-item-delay-3-reverse');
+    nav4.classList.add('slide-in-nav-item-delay-3');
+    nav5.classList.remove('slide-in-nav-item-delay-4-reverse');
+    nav5.classList.add('slide-in-nav-item-delay-4');
   } else {
     // Animate Out - Overlay
-    overlay.classList.remove("overlay-slide-right");
-    overlay.classList.add("overlay-slide-left");
+    overlay.classList.remove('overlay-slide-right');
+    overlay.classList.add('overlay-slide-left');
     // Animate Out - Nav Items
-    nav1.classList.remove("slide-in-nav-item");
-    nav1.classList.add("slide-in-nav-item-reverse");
-    nav2.classList.remove("slide-in-nav-item-delay-1");
-    nav2.classList.add("slide-in-nav-item-delay-1-reverse");
-    nav3.classList.remove("slide-in-nav-item-delay-2");
-    nav3.classList.add("slide-in-nav-item-delay-2-reverse");
-    nav4.classList.remove("slide-in-nav-item-delay-3");
-    nav4.classList.add("slide-in-nav-item-delay-3-reverse");
-    nav5.classList.remove("slide-in-nav-item-delay-4");
-    nav5.classList.add("slide-in-nav-item-delay-4-reverse");
+    nav1.classList.remove('slide-in-nav-item');
+    nav1.classList.add('slide-in-nav-item-reverse');
+    nav2.classList.remove('slide-in-nav-item-delay-1');
+    nav2.classList.add('slide-in-nav-item-delay-1-reverse');
+    nav3.classList.remove('slide-in-nav-item-delay-2');
+    nav3.classList.add('slide-in-nav-item-delay-2-reverse');
+    nav4.classList.remove('slide-in-nav-item-delay-3');
+    nav4.classList.add('slide-in-nav-item-delay-3-reverse');
+    nav5.classList.remove('slide-in-nav-item-delay-4');
+    nav5.classList.add('slide-in-nav-item-delay-4-reverse');
   }
 }
+
+// Event Listeners
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
+nav4.addEventListener('click', toggleNav);
+nav5.addEventListener('click', toggleNav);
