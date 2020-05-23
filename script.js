23d6fbe610d1a8e217e87710a817d5fc1a9e55ad
Jacinto Wong <37252418+JacintoDesign@users.noreply.github.com>
2020-05-23 14:30:00 -0400

    final production code

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
+// Control Navigation Animation
+function navAnimation(direction1, direction2) {
+  nav1.classList.replace(`slide-${direction1}-1`, `slide-${direction2}-1`);
+  nav2.classList.replace(`slide-${direction1}-2`, `slide-${direction2}-2`);
+  nav3.classList.replace(`slide-${direction1}-3`, `slide-${direction2}-3`);
+  nav4.classList.replace(`slide-${direction1}-4`, `slide-${direction2}-4`);
+  nav5.classList.replace(`slide-${direction1}-5`, `slide-${direction2}-5`);
+}
+
 function toggleNav() {
   // Toggle: Menu Bars Open/Closed
   menuBars.classList.toggle('change');
@@ -15,20 +24,12 @@ function toggleNav() {
     // Animate In - Overlay
     overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
     // Animate In - Nav Items
-    nav1.classList.replace('slide-out-1', 'slide-in-1');
-    nav2.classList.replace('slide-out-2', 'slide-in-2');
-    nav3.classList.replace('slide-out-3', 'slide-in-3');
-    nav4.classList.replace('slide-out-4', 'slide-in-4');
-    nav5.classList.replace('slide-out-5', 'slide-in-5');
+    navAnimation('out', 'in');
   } else {
     // Animate Out - Overlay
     overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
     // Animate Out - Nav Items
-    nav1.classList.replace('slide-in-1', 'slide-out-1');
-    nav2.classList.replace('slide-in-2', 'slide-out-2');
-    nav3.classList.replace('slide-in-3', 'slide-out-3');
-    nav4.classList.replace('slide-in-4', 'slide-out-4');
-    nav5.classList.replace('slide-in-5', 'slide-out-5');
+    navAnimation('in', 'out');
   }
 }
 
